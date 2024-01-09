71d96344007473392f52b55c572cdf2aef2e7bf1
Ollie <ohol0007@student.monash.edu>
2024-01-09 12:34:59 +1100

    Database connection organised and posting of workout working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 
@@ -18,10 +19,17 @@ app.use((req, res,next)=> {
 //routes
 app.use('/api/workouts',workoutRoutes)
 
+// connect to db
+mongoose.connect(process.env.MON_URI)
+    .then(() => {
+        //listen for request
+        app.listen(process.env.PORT, ()=> {
+            console.log(' connect to db listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
-// listen for requests
-app.listen(process.env.PORT, ()=> {
-    console.log('listening on port ', process.env.PORT)
-})
 
